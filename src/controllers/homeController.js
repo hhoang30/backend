08b5dbe66821cebdd1a0fad37c47512b0b704fbf
hhoang30 <nguyenhuyhoang21112001@gmail.com>
2024-08-25 23:36:15 +0700

    25/8

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,19 +2,7 @@ const connection = require('../config/database')
 
 
 const getHomepage = (req, res) => {
-    // res.send('Hello me')
-
-    let users = [];
-    
-    connection.query(
-        'SELECT * FROM Users u',
-        function (err, results, fields) {
-            users = results
-            console.log(results); // results contains rows returned by server 
-            
-            res.send(JSON.stringify(users))
-        }
-      );
+    return res.render('home.ejs')
 }
 
 const getHH = (req, res) => {
