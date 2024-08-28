c9ed804d308616d2c8b30331999c29b5b4f522af
hhoang30 <nguyenhuyhoang21112001@gmail.com>
2024-08-29 00:24:34 +0700

    29/8

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,8 +1,10 @@
 const connection = require('../config/database')
+const {getAllUsers} = require('../services/CRUDService')
 
+const getHomepage = async (req, res) => {
 
-const getHomepage = (req, res) => {
-    return res.render('home.ejs')
+    let results = await getAllUsers();
+    return res.render('home.ejs', { listUsers: results })
 }
 
 const getHH = (req, res) => {
@@ -13,28 +15,33 @@ const getCreatePage = (req, res) => {
     return res.render('create.ejs')
 }
 
-const postCreateUser = (req,res) => {
-    console.log(req.body.name);
+const getUpdatePage = (req, res) => {
+    let userId = req.params.userId;
+    console.log(req.params, userId);
+    
+    return res.render('update.ejs')
+}
+
+const postCreateUser = async (req,res) => {
+    // console.log(req.body.name);
     // let email = req.body.email;
     // let name = req.body.name;
     // let city = req.body.city;
 
     let { email, name, city } = req.body;
+    
+    let [results , fields] = await connection.query(
+        `INSERT INTO Users (email, name, city) VALUES (?, ?, ?)`, [email, name, city]
+    );
+    console.log("check result", results);
+    
     res.send("create new user");
-
-    connection.query(
-        `INSERT INTO
-        Users (email, name, city)
-        VALUES (?, ?, ?)`,
-        [email, name, city],
-        function (err, results) {
-          console.log(results);
-        }
-      );
 }
 
 module.exports = {
     getHomepage,
     getHH,
-    postCreateUser,getCreatePage
+    postCreateUser,
+    getCreatePage,
+    getUpdatePage
 }
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router();
-const {getHomepage, getHH, postCreateUser, getCreatePage} = require('../controllers/homeController')
+const {getHomepage, getHH, postCreateUser, getCreatePage, getUpdatePage} = require('../controllers/homeController')
 
 // khai bao route
 router.get('/', getHomepage);
 router.get('/hh', getHH)
 router.get('/create', getCreatePage)
+router.get('/update/:userId', getUpdatePage)
 
 
 router.post('/create-user', postCreateUser)
